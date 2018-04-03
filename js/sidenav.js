f8484ca031a761abf288a8226f4f94989055ba98
T4L3S <mikolaj.s.kaczmarek@outlook.com>
2018-04-03 19:17:07 +0200

    04-03-2018 [19:15]

--- js/sidenav.js
+++ js/sidenav.js
@@ -9,3 +9,13 @@ function closeNav() {
     document.getElementById("sideNav_hidden").style.width = "5%";
     document.getElementById("main").style.marginLeft= "0";
 }
+/*
+$(window).on('load', function () {
+    alert("window loaded");
+    var offsetHeight = document.getElementById('myDiv').offsetHeight;
+   //var height = $("logo_div").Height();
+   alert (("to") + offsetHeight);
+   //document.getElementById("sideNav_hidden").style.height = height;
+   $("#sideNav_hidden").top(height + "px");
+});
+*/
