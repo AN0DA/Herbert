93a6d882ecabb583a9eab3e39d74bfc9510dcb8d
T4L3S <mikolaj.s.kaczmarek@outlook.com>
2018-04-06 00:08:05 +0200

    04-05-2018

--- Script.js
+++ Script.js
@@ -5,12 +5,12 @@ function openNav() {
     
     document.getElementById("logo_div").style.width = "18vw";
     document.getElementById("logo_div").style.height = "18vw";
+    document.getElementById("logo_div").style.borderRadius = "0";
     
     document.getElementById("title").style.marginLeft = "18vw";
     
-    document.getElementById("main").style.marginLeft = "18vw";
+    document.getElementById("main").style.marginLeft = "23.5vw";
 }
-
 function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     document.getElementById("sideNav").style.top = "13vw";
@@ -18,8 +18,32 @@ function closeNav() {
     
     document.getElementById("logo_div").style.width = "13vw";
     document.getElementById("logo_div").style.height = "13vw";
+    document.getElementById("logo_div").style.borderRadius = "0 0 10px 0";
     
     document.getElementById("title").style.marginLeft = "13vw";
     
+<<<<<<< HEAD:Script.js
+    document.getElementById("main").style.marginLeft = "18.5vw";
+}
+
+/* zmiana nav po zescrolowaniu, do zrobienia w wolnej chwili 
+ * 
+$(document).ready(function() {
+    
+  $(window).scroll(function () {
+      
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
+
+*/
+=======
     document.getElementById("main").style.marginLeft = "0";
 }
+>>>>>>> 4785734f949daf5993519868ac56576a18f8e3aa:script.js
